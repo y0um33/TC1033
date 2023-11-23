54c4cf94b6b8fa79c1258df1eb0d026465e1ee8a
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2023-11-22 22:26:39 -0600

    Create juego.h

---
+++ juego.h
@@ -0,0 +1,29 @@
+#ifndef JUEGO.H
+#define JUEGO.H
+
+#include <string>
+#include "producto.h"
+using namespace std;
+
+class game : public producto{
+private:
+    string description;
+
+public:
+    game(): description(""){};
+    game(float pre, string ori, string des): producto(pre,ori), description(des){};
+
+    string get_description();
+
+    void set_description(string);
+};
+
+string game::get_description(){
+    return description;
+}
+
+void game::set_description(string des){
+    description = des;
+};
+
+#endif
