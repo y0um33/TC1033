d79a71e9b811e1a6222745047fb5dd380f3233c8
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2023-11-22 22:26:11 -0600

    Update fruta.h

--- fruta.h
+++ fruta.h
@@ -1,13 +1,17 @@
+#ifndef FRUTA_H
+#define FRUTA_H
+
 #include <string>
+#include "producto.h"
 using namespace std;
 
-class fruta{
+class fruta : public producto{
 private:
     int cantidad;
    
 public:
     fruta(): cantidad(0){};
-    fruta(int can): cantidad(can){};
+    fruta(float pre, string ori, int can): producto(pre,ori), cantidad(can){};
 
     int get_cantidad();
 
@@ -23,3 +27,4 @@ void fruta::set_cantidad(int can){
     cantidad = can;
 };
 
+#endif
