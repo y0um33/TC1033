fd2abdae1cc48e71383c0ce20878eaf45aab85a2
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2023-11-29 14:39:34 -0600

    Update season.h

--- season.h
+++ season.h
@@ -1,3 +1,14 @@
+/*
+    Yumee Chung
+    A01712059
+    30/11/2023
+*/
+
+/*
+    El clase season se hace para guardar y acceder a diferente periodo. El clase se
+    va ser composionado en descuento.h para acceder al periodo. 
+*/
+
 #ifndef SEASON_H
 #define SEASON_H
 
@@ -6,17 +17,17 @@ using namespace std;
 
 class season{
 private: 
-    string period;
+    string period; //objeto period
 public:
-    season(): period(""){};
+    season(): period(""){}; //constructor
     season(string per): period(per){};
 
-    string get_period();
-    void set_period(string);
+    string get_period(); //Variable para poder mandarlo a Main y llamar lo que está guardado
+    void set_period(string); //Variable para poder mandarlo a Main y asignar el valor
 };
 
 string season::get_period(){
-    return period;
+    return period; //return period (string)
 }
 
 void season::set_period(string per){
