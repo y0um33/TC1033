217635467485d8a884052fbdd756d146e5625ee3
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2023-11-29 13:37:13 -0600

    Update fruta.h

--- fruta.h
+++ fruta.h
@@ -1,3 +1,14 @@
+/*
+    Yumee Chung
+    A01712059
+    30/11/2023
+*/
+
+/*
+    El clase fruta hereda de clase producto. Tiene su propio objeto cantidad. Guarda la cantidad
+    de las frutas y asigna valores a través de getter y setter
+*/
+
 #ifndef FRUTA_H
 #define FRUTA_H
 
@@ -5,26 +16,26 @@
 #include "producto.h"
 using namespace std;
 
-class fruta : public producto{
+class fruta : public producto{ //Clase fruta hereda de público de producto.h
 private:
-    int cantidad;
+    int cantidad; //objeto cantidad
    
 public:
-    fruta(): cantidad(0){};
+    fruta(): cantidad(0){}; //constructor
     fruta(float pre, string ori, int can): producto(pre,ori), cantidad(can){};
 
-    int get_cantidad();
+    int get_cantidad(); //Variable para poder mandarlo a Main y llamar lo que está guardado
 
-    void set_cantidad(int);
+    void set_cantidad(int); //Variable para poder mandarlo a Main y asignar el valor
     
 };
 
-int fruta::get_cantidad(){
-    return cantidad;
+int fruta::get_cantidad(){ //una solicitud de description que está private, acceder
+    return cantidad; //return cantidad
 }
 
 void fruta::set_cantidad(int can){
-    cantidad = can;
+    cantidad = can; //Declaración que can es igual a cantidad
 };
 
 #endif
