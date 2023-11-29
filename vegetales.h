06a63058b24f75a0901819d5e6d2ac05304bdb35
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2023-11-29 13:44:06 -0600

    Update vegetales.h

--- vegetales.h
+++ vegetales.h
@@ -1,3 +1,14 @@
+/*
+    Yumee Chung
+    A01712059
+    30/11/2023
+*/
+
+/*
+    El clase vegetales hereda de clase producto. Tiene su propio objeto cantidad. Guarda la cantidad
+    de las vegetales y asigna valores a través de getter y setter
+*/
+
 #ifndef VEGETALES_H
 #define VEGETALES_H
 
@@ -5,25 +16,25 @@
 #include "producto.h"
 using namespace std;
 
-class vegetal : public producto{
+class vegetal : public producto{ //clase vegetal hereda de público de producto
 private:
-    int value;
+    int value; //objeto de value
 
 public:
-    vegetal(): value(0){};
+    vegetal(): value(0){}; //constructor 
     vegetal(float pre, string ori, int va): producto(pre,ori), value(va){};
 
-    int get_value();
+    int get_value(); //Variable para poder mandarlo a Main y llamar lo que está guardado
 
-    void set_value(int);
+    void set_value(int); //Variable para poder mandarlo a Main y asignar el valor
 };
 
-int vegetal::get_value(){
-    return value;
+int vegetal::get_value(){ //una solicitud de value que está private, acceder
+    return value; //return value
 }
 
 void vegetal::set_value(int va){
-    value = va;
+    value = va; //Declaración que va es igual a value
 };
 
 #endif
